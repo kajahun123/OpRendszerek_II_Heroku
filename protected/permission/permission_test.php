997d100670ef3a3afa852f1dfc5a81e5b9abadc1
kajahun123 <krajcsimarci@gmail.com>
2021-01-16 16:54:45 +0100

    Update permission_test.php

--- protected/permission/permission_test.php
+++ protected/permission/permission_test.php
@@ -1,4 +1,9 @@
 <?php if(!isset($_SESSION['permission']) || $_SESSION['permission'] < 1) : ?>
+<style>
+    h1{
+        color: #7ac4ac;
+    }
+</style>
 	<h1>Belépve userként</h1>
 	
 <?php else : ?>
